1fa36862da56902ea28063fef46fc63d5eb4a8a2
Haialigpo <165869181+Haialigpo@users.noreply.github.com>
2024-06-18 11:17:52 +0530

    Update product-list.js
    update

--- blocks/product-list/product-list.js
+++ blocks/product-list/product-list.js
@@ -12,7 +12,7 @@ function initProductListComponents() {
         productListHTML += `
                           <div class="product-item">
                              <a href="${`pdp?product=${product.sku}`}" aria-label="View details for ${product.name}">
-                              <img src="${`https://assets.unileversolutions.com/v1/${product.thumbnail.url}.png?im=AspectCrop=(400,400);Resize=(400,400)`}" alt="${product.thumbnail.label}">
+                              <img src="${`https://placehold.jp/400x400.png`}" alt="${product.altText}">
                                <h2>${product.name}</h2>
                               <div class="product-description">${product.description.html}</div>
                               </a>
@@ -26,72 +26,20 @@ function initProductListComponents() {
     const { size, apiurl, store } = div.dataset;
     // Extract data attributes from the div
     const fetchAndDisplayProducts = (page = 1) => {
-      const headers = {
-        size,
-        store,
-        'Content-Type': 'application/json',
-      };
-      //  const loadmoreButtonText = div.dataset.loadmorebutton;
 
-      const query = `
-        query {
-          products(currentPage: ${page}, pageSize: ${size}, filter: {category_uid: {eq: "{\\"sort_by\\":\\"relevance\\"}"}}, sort: {price: ASC}) {
-            total_count
-            items {
-              __typename
-              sku
-              name
-              description {
-                html
-              }
-              image {
-                label
-                url
-              }
-              thumbnail {
-                label
-                url
-              }
-              url_key
-              url_rewrites {
-                url
-              }
-              media_gallery {
-                __typename
-                disabled
-                url
-                label
-                position
-              }
-              categories {
-                __typename
-                id
-                name
-                image
-              }
-              smartProductID_custom_: smartProductID
-              localized_sku_custom_: localized_sku
-              brand_custom_: brand
-              category_custom_: category
-              productVariants_custom_: custom_retailerVariantsSummary
-            }
-          }
-        }`;
-
-      fetch(apiurl, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({ query }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          createProductList(data.data.products.items);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    };
+    fetch("./product.json")
+               .then((res) => {
+                   if (!res.ok) {
+                       throw new Error
+                           (`HTTP error! Status: ${res.status}`);
+                   }
+                    createProductList(res.data.products.items);
+               })
+               .then((data) => 
+                     console.log(data))
+               .catch((error) => 
+                      console.error("Unable to fetch data:", error));
+       }
     fetchAndDisplayProducts();
   });
 }
